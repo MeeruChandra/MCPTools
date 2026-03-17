7654ab6593105f9cc2603468e236987e13986f75
MeeruChandra <meetuchandra@gmail.com>
2026-03-17 17:12:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,3 @@ mcp = FastMCP("my_echo_server")
 def echo(text: str) -> str:
     return text + " From My Test server"
 
-if __name__ == "__main__":
-    mcp.run(
-        transport="http",
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000))
-    )
